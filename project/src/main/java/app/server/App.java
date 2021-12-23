2ec78c83e63dbab98802b0a3f07e01fe2cb41e79
Jinglong-Liu <1601029287@qq.com>
2021-12-23 21:08:14 +0800

    v1.1

--- project/src/main/java/app/server/App.java
+++ project/src/main/java/app/server/App.java
@@ -1,9 +1,9 @@
-package app.client;
+package app.server;
 
 import javafx.application.Application;
 
 public class App{
     public static void main(String[] args) {
-        Application.launch(Main.class);
+        Application.launch(ServerApp.class);
     }
 }
